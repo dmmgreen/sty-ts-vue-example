99d98fb4752922bab9ec309b6dbf86b1bd3d229d
liaoss <web_developer@amicloud.cn>
2021-03-22 17:23:02 +0800

    新增页面

---
+++ src/views/table/complex-table.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="app-container">
+    <el-table>
+      <el-table-column>
+        <template>
+          <span>test</span>
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
+</template>
